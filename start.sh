047a727137288e2038b3ddd4aa77bedd757e4c50
Joseph Giovanelli <joseph.giovanelli@studio.unibo.it>
2022-04-18 19:08:30 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-docker stop effective_preprocessing_pipeline
-docker rm effective_preprocessing_pipeline
-docker build -t effective_preprocessing_pipeline .
-docker run -it --name effective_preprocessing_pipeline -v $(pwd):/home effective_preprocessing_pipeline 
+docker stop effective_preprocessing_pipelines
+docker rm effective_preprocessing_pipelines
+docker build -t effective_preprocessing_pipelines .
+docker run -it --name effective_preprocessing_pipelines -v $(pwd):/home effective_preprocessing_pipelines
