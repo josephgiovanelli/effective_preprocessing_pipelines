014409f4a24beae0e9c64b3a3e9c45f186fbb19a
Joseph Giovanelli <j.giovanelli@unibo.it>
2022-06-08 14:44:18 +0200

    Update wrapper_experiments.sh

--- wrapper_experiments.sh
+++ wrapper_experiments.sh
@@ -3,8 +3,8 @@ TOY_EXAMPLE=true
 
 ./scripts/pipeline_impact.sh $TOY_EXAMPLE
 
-# ./scripts/pipeline_construction.sh $TOY_EXAMPLE
+./scripts/pipeline_construction.sh $TOY_EXAMPLE
 
-# ./scripts/evaluation.sh $TOY_EXAMPLE
+./scripts/evaluation.sh $TOY_EXAMPLE
 
-# ./scripts/exploratory_analysis.sh $TOY_EXAMPLE
+./scripts/exploratory_analysis.sh $TOY_EXAMPLE
