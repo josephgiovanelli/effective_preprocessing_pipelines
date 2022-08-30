9f11f2d1a4b6197810722d197a18b9034969a397
Joseph Giovanelli <j.giovanelli@unibo.it>
2022-08-30 12:01:36 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,4 @@
 docker stop effective_preprocessing_pipelines
 docker rm effective_preprocessing_pipelines
-docker build -t effective_preprocessing_pipelines .
-docker run --name effective_preprocessing_pipelines --volume e:/effective_preprocessing_pipelines:/home --detach -t effective_preprocessing_pipelines
+docker run --name effective_preprocessing_pipelines --volume e:/effective_preprocessing_pipelines:/home --detach -t ghcr.io/josephgiovanelli/effective-preprocessing-pipelines:0.1.0
 docker exec effective_preprocessing_pipelines bash ./wrapper_experiments.sh $1
