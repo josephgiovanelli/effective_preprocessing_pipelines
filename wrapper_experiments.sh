019f30160088b9c8df20267368190cd404541506
Joseph Giovanelli <j.giovanelli@unibo.it>
2022-09-02 12:48:14 +0200

    Name refactor

--- wrapper_experiments.sh
+++ wrapper_experiments.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-./scripts/pipeline_impact.sh $1
+./scripts/prototype_construction.sh $1
 
-./scripts/pipeline_construction.sh $1
-
-./scripts/evaluation.sh $1
+./scripts/empirical_evaluation.sh $1
 
 ./scripts/exploratory_analysis.sh $1
