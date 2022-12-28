fed1bf0aa672969ba95de5f9f8aa01844ec2054a
Joseph Giovanelli <41596745+josephgiovanelli@users.noreply.github.com>
2022-12-28 14:52:45 +0100

    Update wrapper_experiments.sh

--- scripts/wrapper_experiments.sh
+++ scripts/wrapper_experiments.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-./exploratory_analysis.sh $1 $2
+./scripts/exploratory_analysis.sh $1 $2
 
-./prototype_construction.sh $1 $2
+./scripts/prototype_construction.sh $1 $2
 
-./experimental_evaluation.sh $1 $2
+./scripts/experimental_evaluation.sh $1 $2
